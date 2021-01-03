2cdc11c73da46823bc88ee61ace61b57838f952b
AlexBirdSong <alexwhitechoco@gmail.com>
2021-01-03 14:36:31 +0300

    learn import/export components

---
+++ my/src/Footer.js
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <div>
+      Pozhiloy footer -_-
+    </div>
+  );
+}
+  export default Footer;
---
+++ my/src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <a href="#">Home </a> 
+      <a href="#">New Feed </a>
+      <a href="#">Messages </a>
+    </div>
+  );
+}
+  export default Header;
